3f6de41a02c9f452b7f22b5f902ac83d9dc0acea
nikitasmith38-cmyk <nikitasmith38@gmail.com>
2026-06-03 01:44:51 -0400

    EC2 instance and key pair

--- compute.tf
+++ compute.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "workforceconnect-app-server" {
   ami           = "ami-0eab37cfdc33e8e65"
   instance_type = "t3.micro"
   subnet_id     = "subnet-0388f6a18fe7661bf"
-  vpc_security_group_ids = [aws_security_group.ec2.id]
+  vpc_security_group_ids = [sg-063e7d215c2df6855]
   key_name      = "workforceconnect-key"
 
 # Assigns a public IP — required for SSH and HTTP access
