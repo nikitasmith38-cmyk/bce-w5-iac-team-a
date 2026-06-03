bf2c692b427f2322c3036f5bcf04e20801bc45f0
nikitasmith38-cmyk <nikitasmith38@gmail.com>
2026-06-03 00:50:27 -0400

    EC2 instance and key pair

--- compute.tf
+++ compute.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "main" {
 
 # EC2 Instance
 resource "aws_instance" "workforceconnect" {
-  ami           = "ami-0c55b159cbfafe1f0" 
+  ami           = "ami-0b9064170e32bde34"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id 
   vpc_security_group_ids = [aws_security_group.ec2.id]
