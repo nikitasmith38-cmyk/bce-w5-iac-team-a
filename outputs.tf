322647f86b1158f4ba0ab6a6a1e438f35556dfe7
nikitasmith38-cmyk <nikitasmith38@gmail.com>
2026-06-02 20:55:02 -0400

    Output values shown after apply
    Add outputs for VPC, EC2, and RDS instances.

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,25 @@
+output "vpc_id" {
+  description = "ID of the deployed VPC"
+  value       = aws_vpc.main.id
 
+}
+
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.workforceconnect.public_ip
+}
+
+output "ec2_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.workforceconnect.public_dns
+}
+
+output "rds_endpoint" {
+  description = "Connection endpoint for the RDS instance"
+  value       = aws_db_instance.main.address
+}
+
+output "rds_port" {
+  description = "Port for the RDS instance"
+  value       = aws_db_instance.main.port
+}
