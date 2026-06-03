0e8b4033cd1abeafcaa274a8532716028fe10b54
nikitasmith38-cmyk <nikitasmith38@gmail.com>
2026-06-03 16:49:42 -0400

    RDS subnet group and RDS instance

--- database.tf
+++ database.tf
@@ -18,8 +18,8 @@ resource "aws_db_instance" "main" {
   storage_type      = "gp2"
   
   db_name  = var.db_name
-  username = var.db_username
-  password = var.db_password
+  username = local.db_creds["admin"]
+  password = local.db_creds["Hollins06%40"]
 
   db_subnet_group_name = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
